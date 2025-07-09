50b017421d619c106f33de75f5e05d3685d22e02
Violeta Carvalho <violetadecarvalho@gmail.com>
2025-07-09 17:09:20 -0300

    Saving score

---
+++ scripts/savedata.gd
@@ -0,0 +1,20 @@
+class_name SaveData extends Resource
+
+@export var high_score: float = 0.0
+
+const SAVE_PATH = "user://save_data.tres"
+
+func save():
+	ResourceSaver.save(self, SAVE_PATH)
+	
+static func load_or_create():
+	var res: SaveData
+	
+	if FileAccess.file_exists(SAVE_PATH):
+		res = load(SAVE_PATH) as SaveData
+	else:
+		res = SaveData.new()
+		res.high_score = 0
+		
+	return res
+	
